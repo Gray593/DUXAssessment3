a74fb156c1dd981826389fcb3dccb74f638ec43d
Gray <btgray6@gmail.com>
2025-04-29 15:55:46 +0100

    added to main

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ export default function App() {
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render();
+root.render(App);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
