162243f5843b64db8e54281d69c0d34447dd6c5e
Gray <btgray6@gmail.com>
2025-05-01 19:22:51 +0100

    page title changed

--- src/Tile.js
+++ src/Tile.js
@@ -3,22 +3,14 @@ import { Link } from "react-router-dom";
 export default function Trending({name,src,link,overview}){
 
     return(         
-<Link
-  to={link}
-  id={name}
-  className="tile"
-  style={{
-    backgroundImage: `url(${src})`,
-  }}
->
-  <h2 className="tileTitle">{name}</h2>
-
-  <div className="overview">
-    <h2 className="overviewTitle">{name}</h2>
-    <h3 className="overviewSubtitle">Overview</h3>
-    <p className="overviewPara">{overview}</p>
-  </div>
-</Link>
+        <Link to={link} id={name} className="tile" style={{backgroundImage: `url(${src})`}}>
+            <h2 className="tileTitle">{name}</h2>
+            <div className="overview">
+                <h2 className="overviewTitle">{name}</h2>
+                <h3 className="overviewSubtitle">Overview</h3>
+                <p className="overviewPara">{overview}</p>
+            </div>
+        </Link>
 
     )
 
